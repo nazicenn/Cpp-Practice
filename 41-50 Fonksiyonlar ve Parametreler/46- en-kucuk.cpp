f00b331fa99b6975808c6ab271a1f61e80c7ed84
Naz İçen <icennaz354@gmail.com>
2026-04-03 00:17:08 +0300

    Add function to find the smaller of two numbers

---
+++ 41-50 Fonksiyonlar ve Parametreler/46- en-kucuk.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std; 
+   int enbuyuk (int a, int b) {
+    if (a < b) 
+        return a;
+    else 
+        return b; 
+   }
+
+int main () {
+    int sayi1;
+    int sayi2;
+
+    cout << "sayi giriniz : " ; 
+    cin >> sayi1;
+    
+    cout << "sayi giriniz : " ; 
+    cin >> sayi2;
+    
+    int sonuc = enbuyuk (sayi1, sayi2);
+
+    cout << "En buyuk sayi: " << sonuc << endl;
+
+    
+    return 0;
+}
